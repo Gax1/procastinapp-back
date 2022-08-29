edf7d2bdb58f3b245d71fc1226aad476ba7f8aee
Gabriel Andres <gabrielleoandres@gmail.com>
2022-08-29 20:17:30 +0200

    Added test for the general error middleware

--- src/server/middlewares/errors.test.ts
+++ src/server/middlewares/errors.test.ts
@@ -1,5 +1,7 @@
-import { Request, Response } from "express";
-import { notFoundError } from "./errors";
+import { NextFunction, Request, Response } from "express";
+import CustomError from "../../interfaces/interfaces";
+import customErrorGenerator from "../../utils/customErrorGenerator";
+import { generalError, notFoundError } from "./errors";
 
 describe("Given a notFound middleware", () => {
   describe("When it receives a request obect", () => {
@@ -27,6 +29,55 @@ describe("Given a notFound middleware", () => {
       notFoundError(req as Request, res as Response);
 
       expect(res.json).toHaveBeenCalledWith({ Error: publicMessage });
+    });
+  });
+});
+
+describe("Given a general error middleware", () => {
+  describe("When it receives a request object", () => {
+    const statusCode = 409;
+    const errorMessage = "Error for the client";
+    const privateMessage = "Error for the developer";
+
+    const error = customErrorGenerator(
+      statusCode,
+      privateMessage,
+      errorMessage
+    );
+
+    test("Then it should call the response status method with an error code", () => {
+      const req = {} as Partial<Request>;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as Partial<Response>;
+      const next: NextFunction = jest.fn();
+
+      generalError(
+        error as CustomError,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+    test("Then it should call the json method with the message error", () => {
+      const req = {} as Partial<Request>;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({ Error: errorMessage }),
+      } as Partial<Response>;
+      const next: NextFunction = jest.fn();
+
+      generalError(
+        error as CustomError,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ Error: errorMessage });
     });
   });
 });
