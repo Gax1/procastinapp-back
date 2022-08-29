88cbf6c2408a6160667a16bcc547930813fc64e9
Gabriel Andres <gabrielleoandres@gmail.com>
2022-08-29 17:38:53 +0200

    Added function to create server and listen up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import dataBaseConnection from "./database";
+import "./dotenv";
+import startServer from "./server/startServer";
 
+const port = +process.env.PORT;
+
+const mongoUrl = process.env.MONGOURL as string;
+
+(async () => {
+  try {
+    await dataBaseConnection(mongoUrl);
+    await startServer(port);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
