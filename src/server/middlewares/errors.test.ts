ec4f22c6f15045140271ee642a8e11277d0f04c5
Gabriel Andres <gabrielleoandres@gmail.com>
2022-08-29 20:39:38 +0200

    Tested empy request object in generalError middleware

--- src/server/middlewares/errors.test.ts
+++ src/server/middlewares/errors.test.ts
@@ -80,4 +80,53 @@ describe("Given a general error middleware", () => {
       expect(res.json).toHaveBeenCalledWith({ Error: errorMessage });
     });
   });
+
+  describe("When it receives an error with an empy message and undefined statusCode", () => {
+    const statusCode: undefined = undefined;
+    const publicMessage = "";
+    const privateMessage = "Error for the developer";
+
+    const generalStatusCode = 500;
+    const generalMessage = "Error in the server, please try again later";
+
+    const error = customErrorGenerator(
+      statusCode,
+      privateMessage,
+      publicMessage
+    );
+    test("Then it should call the status method with a 500", () => {
+      const req = {} as Partial<Request>;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as Partial<Response>;
+      const next: NextFunction = jest.fn();
+
+      generalError(
+        error as CustomError,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(generalStatusCode);
+    });
+    test("Then it should call the json method with a message", () => {
+      const req = {} as Partial<Request>;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({ Error: generalMessage }),
+      } as Partial<Response>;
+      const next: NextFunction = jest.fn();
+
+      generalError(
+        error as CustomError,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ Error: generalMessage });
+    });
+  });
 });
